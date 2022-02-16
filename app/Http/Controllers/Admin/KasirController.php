cbdade196f70b5c9f14b0e8f12e346def983d429
ilhamrmdhaaan <ilhamrmdhaaan@gmail.com>
2022-02-16 16:17:24 +0700

    kasir

--- app/Http/Controllers/Admin/KasirController.php
+++ app/Http/Controllers/Admin/KasirController.php
@@ -2,9 +2,7 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Http\Controllers\Controller;
 use App\Models\Kasir;
-use App\Repositories\Interfaces\KasirInterface;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
